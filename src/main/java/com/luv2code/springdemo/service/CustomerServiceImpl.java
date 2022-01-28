2b347ccaa63d1928678dc0420534ee449fff6de2
GitPiotrUrbanowicz <55048144+piotr1202@users.noreply.github.com>
2022-01-28 16:12:20 +0100

    config file
    opening customer-list file without downloading customers

---
+++ src/main/java/com/luv2code/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo.service;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+
+public class CustomerServiceImpl implements CustomerService {
+	
+	private CustomerDAO customerDAO;//czemu interface 
+
+	
+}
